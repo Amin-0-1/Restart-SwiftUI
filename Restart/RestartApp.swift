b59dc1f0b09f4d1fe6cd1ca028ca1dfaab62da83
Amin <m.amin@sweater.sa>
2023-05-22 19:45:56 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Amin on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
