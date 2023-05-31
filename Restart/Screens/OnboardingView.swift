f063e64c1f26c70f4ee5a9a422a2549ea705fc61
Amin <mahmoudamin373@gmail.com>
2023-05-31 18:31:29 +0300

    onboarding
    * adding Dragging Button
    * adding animation

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    // MARK: - Properties
     @AppStorage("Onboarding") var isOnboardingViewActive = true
+    @State private var buttonWidht:Double = UIScreen.main.bounds.width  - 80
+    @State private var buttonOffset:CGFloat = 0
+    @State private var isAnimating = false
+    
     var body: some View {
         ZStack {
             Color("ColorBlue")
@@ -32,6 +37,9 @@ struct OnboardingView: View {
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
                 }
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
                 
                 // MARK: - Center
                 
@@ -48,6 +56,8 @@ struct OnboardingView: View {
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
                 }
                 Spacer()
                 
@@ -64,25 +74,26 @@ struct OnboardingView: View {
                         .padding(8)
                     
                     
-                    // 2. Call-to-action (static)
-                    Text("Get Started")
-                        .font(.system(.title3,design: .default))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .offset(x:20)
                     
                     
                     // 3. Capsule (Dynamic width)
                     HStack{
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80, alignment: .leading)
+                            .frame(width: buttonOffset + 80, alignment: .center)
                         
                         Spacer()
                         
                     }
-                    // 4. Circle (Draggable )
                     
+                    // 2. Call-to-action (static)
+                    Text("Get Started")
+                        .font(.system(.title3,design: .default))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    
+                    // 4. Circle (Draggable )
                     HStack {
                         ZStack{
                             Capsule()
@@ -97,15 +108,39 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ value in
+                                    if value.translation.width > 0{
+                                        buttonOffset = min(buttonWidht - 80, value.translation.width)
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    withAnimation(.easeOut(duration: 1)) {
+                                        if buttonOffset > buttonWidht / 2 {
+                                            buttonOffset = buttonWidht - 80
+                                            isOnboardingViewActive = false
+                                        }else{
+                                            buttonOffset = 0
+                                        }
+                                    }
+                                })
+                                   
+                        )
                         Spacer()
                     }
                 }
-                .frame(height: 80,alignment: .center)
+                .frame(width: buttonWidht,height: 80)
                 .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+
             }
+        }
+        .onAppear {
+            isAnimating = true
         }
     }
 }
